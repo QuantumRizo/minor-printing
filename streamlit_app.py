0a18d38bb8379a28a4406ace52248badf352a241
QuantumRizo <davidrizo.phys@gmail.com>
2026-06-12 12:23:15 -0400

    Resize ron image

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ if uploaded_file is not None:
                     pdf_bytes = f.read()
                 
                 st.success("PDF generated successfully!")
-                st.image("ron.png")
+                st.image("ron.png", width=150)
                 st.download_button(
                     label="⬇️ Download PDF",
                     data=pdf_bytes,
